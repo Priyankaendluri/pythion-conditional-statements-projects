9cdd95743d14cb07207e5dc0a4ae92da887d7254
Priyankaendluri <endlurupriyanka@gmail.com>
2025-01-30 21:52:24 +0530

    Add files via upload

---
+++ b.py
@@ -0,0 +1,39 @@
+'''light=(input("enter your light"))
+if (light=="red"):
+     print("stop")
+elif (light=="green"):
+     print("go");
+elif (light=="yellow"):
+     print("wait")
+else:
+     print("light is broken")'''
+'''m=int(input("Enter your marks"))
+if(m<40):
+    print("fail")
+elif(m>40&m<49):
+    print("E")
+elif(m>50&m<59):
+    print("D")
+elif(m>60&m<69):
+    print("c")
+elif(m>70&m<79):
+    print("B")
+elif(m>80&m<89):
+    print("A")
+elif(m>90):
+    print("EX")
+else:
+    print("error")'''
+'''a=int(input("enteryour age:"))
+g=input("Enter your gender:")
+if (a==1 and g=="f"):
+    print("fee is 100")
+elif (a==2 and g=="f"):
+    print("fee is 300")
+else:
+    print("error")'''
+a=int(input("enter a number"))
+if(a%2==0):
+    print("even")
+else:
+    print("odd")
